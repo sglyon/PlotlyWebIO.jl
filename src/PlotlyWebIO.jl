2e44152ea394a32bef465e4b0897a6e72d310d32
Spencer Lyon <spencerlyon2@gmail.com>
2018-01-22 09:52:59 -0500

    ENH: more wip stuff on webio. Trying to organize IMPORTANT (see below)
    the event code here _will not_ work without some adjustments I've made
    to the WebIO javascript. You shouldn't see any errors on the javascript
    side, but you just won't ever receive any event data

--- src/PlotlyWebIO.jl
+++ src/PlotlyWebIO.jl
@@ -10,28 +10,139 @@ using WebIO
 import WebIO.render
 using JSON
 
-mutable struct WebIOPlot
-    p::PlotlyJS.Plot
-    svg::String
-    api_obs::Dict{Any,Any}
+do_nothing(args...) = nothing
+log_args(args...) = (@show args; nothing)
+
+mutable struct PlotlyEvents
+    plotly_click
+    plotly_hover
+    plotly_unhover
+    plotly_selecting
+    plotly_selected
 end
 
-WebIOPlot(args...; kwargs...) = WebIOPlot(Plot(args...; kwargs...), "", Dict())
+function PlotlyEvents()
+    PlotlyEvents(do_nothing, do_nothing, do_nothing, do_nothing, do_nothing)
+end
+
+function log_events()
+    PlotlyEvents(log_args, log_args, log_args, log_args, log_args)
+end
+
+mutable struct WebIOPlot
+    p::PlotlyJS.Plot
+    widget::Widget
+    svg::String
+    api_obs::Dict{String,Observable}
+    event_functions::PlotlyEvents
+    event_data::Dict{String,Dict}
+    event_obs::Dict{String,Observable}
+end
+
+function setup_api_obs(p::PlotlyJS.Plot, widget::Widget)
+    svg_obs = Observable(widget, "svg-string", "")
+    api_obs = Dict{String,Observable}("svg" => svg_obs)
+    id = string("#plot-", p.divid)
+
+    # set up restyle
+    restyle_obs = Observable(widget, "restyle_args", RestyleArgs())
+    api_obs["restyle"] = restyle_obs
+
+    onjs(restyle_obs, @js function(val)
+        @var gd = this.dom.querySelector($id);
+        Plotly.restyle(gd, val.data, val.traces).then(function(gd)
+            Plotly.toImage(gd, $(Dict("format" => "svg")))
+        end
+        ).then(function(data)
+            @var svg_data = data.replace("data:image/svg+xml,", "")
+            $svg_obs[] = decodeURIComponent(svg_data)
+        end
+        );
+    end)
+
+    # set up relayout
+    relayout_obs = Observable(widget, "relayout_args", RelayoutArgs())
+    api_obs["relayout"] = relayout_obs
+
+    onjs(relayout_obs, @js function(val)
+        @var gd = this.dom.querySelector($id);
+        Plotly.relayout(gd, val.data).then(function(gd)
+            Plotly.toImage(gd, $(Dict("format" => "svg")))
+        end
+        ).then(function(data)
+            @var svg_data = data.replace("data:image/svg+xml,", "")
+            $svg_obs[] = decodeURIComponent(svg_data)
+        end
+        );
+    end)
+
+    # set up update
+    update_obs = Observable(widget, "update_args", UpdateArgs())
+    api_obs["update"] = update_obs
+
+    onjs(update_obs, @js function(val)
+        @var gd = this.dom.querySelector($id);
+        Plotly.update(gd, val.data, val.layout, val.traces).then(function(gd)
+            Plotly.toImage(gd, $(Dict("format" => "svg")))
+        end
+        ).then(function(data)
+            @var svg_data = data.replace("data:image/svg+xml,", "")
+            $svg_obs[] = decodeURIComponent(svg_data)
+        end
+        );
+    end)
+
+    # TODO: addtraces, deletetraces, movetraces, redraw, purge, to_image,
+    # download_image, extendtraces, prependtraces
+
+    api_obs
+end
+
+function bind_events!(
+        widget::Widget,
+        event_obs::Dict{String,Observable}, event_data::Dict{String,Dict},
+        event_functions::PlotlyEvents
+    )
+    # set up observables for event data
+    for name in ["hover", "selected", "click", "relayout"]
+        event_obs[name] = Observable(widget, string("on-", name), Dict())
+        event_data[name] = Dict()
+        on(data -> setindex!(event_data, data, name), event_obs[name])
+    end
+    # TODO: hook up event_functions
+end
+
+function WebIOPlot(args...; events::PlotlyEvents=PlotlyEvents(), kwargs...)
+    p = Plot(args...; kwargs...)
+    # TODO: figure out how to load locally in IJulia)
+    # widget = Widget(dependencies=["/pkg/PlotlyJS/plotly-latest.min.js"])
+    widget = Widget(dependencies=["https://cdn.plot.ly/plotly-latest.min.js"])
+    api_obs = setup_api_obs(p, widget)
+
+    event_data = Dict{String,Dict}()
+    event_obs = Dict{String,Observable}()
+    bind_events!(widget, event_obs, event_data, events)
+
+    WebIOPlot(p, widget, "", api_obs, events, event_data, event_obs)
+end
+
 
 function render(p::WebIOPlot)
-    # widget = Widget(dependencies=["/pkg/PlotlyJS/plotly-latest.min.js"])
-    # TODO: figure out how to load locally in IJulia
-    widget = Widget(dependencies=["https://cdn.plot.ly/plotly-latest.min.js"])
-    svg_obs = Observable(widget, "svg-string", "")
-    on(svg_obs) do svg
-        p.svg = svg
-    end
-
     lowered = JSON.lower(p.p)
     options = Dict("showLink"=> false)
 
-    ondependencies(widget, WebIO.@js function (Plotly)
-        @var gd = this.dom.querySelector("#theplot");
+    id = string("#plot-", p.p.divid)
+
+    # unpack Observables so we can hook them up in our js below
+    svg_obs = p.api_obs["svg"]
+    hover_obs = p.event_obs["hover"]
+    selected_obs = p.event_obs["selected"]
+    click_obs = p.event_obs["click"]
+    relayout_obs = p.event_obs["relayout"]
+
+    ondependencies(p.widget, WebIO.@js function (Plotly)
+
+        @var gd = this.dom.querySelector($id);
         Plotly.newPlot(
             gd, $(lowered[:data]), $(lowered[:layout]), $(options)
         ).then(function(gd)
@@ -45,60 +156,45 @@ function render(p::WebIOPlot)
         window.onresize = function()
             Plotly.Plots.resize(gd)
         end
+
+        gd.on("plotly_hover", function (data)
+            @var filtered_data = WebIO.CommandSets.Plotly.filterEventData(gd, data, "hover");
+            if !(filtered_data.isnil)
+                $hover_obs[] = filtered_data.out
+            end
+        end)
+
+        gd.on("plotly_unhover", function (data)
+            $hover_obs[] = $(Dict())
+        end)
+
+        gd.on("plotly_selected", function (data)
+            @var filtered_data = WebIO.CommandSets.Plotly.filterEventData(gd, data, "selected");
+            if !(filtered_data.isnil)
+                $selected_obs[] = filtered_data.out
+            end
+        end)
+
+        gd.on("plotly_deselect", function (data)
+            $selected_obs[] = $(Dict())
+        end)
+
+        gd.on("plotly_relayout", function (data)
+            @var filtered_data = WebIO.CommandSets.Plotly.filterEventData(gd, data, "relayout");
+            if !(filtered_data.isnil)
+                $relayout_obs[] = filtered_data.out
+            end
+        end)
+
+        gd.on("plotly_click", function (data)
+            @var filtered_data = WebIO.CommandSets.Plotly.filterEventData(gd, data, "click");
+            if !(filtered_data.isnil)
+                click_obs[] = filtered_data.out
+            end
+        end)
     end)
 
-    # set up restyle
-    restyle_obs = Observable(widget, "restyle_args", RestyleArgs())
-    p.api_obs["restyle"] = restyle_obs
-
-    onjs(restyle_obs, @js function(val)
-        @var gd = this.dom.querySelector("#theplot");
-        Plotly.restyle(gd, val.data, val.traces).then(function(gd)
-            Plotly.toImage(gd, $(Dict("format" => "svg")))
-        end
-        ).then(function(data)
-            @var svg_data = data.replace("data:image/svg+xml,", "")
-            $svg_obs[] = decodeURIComponent(svg_data)
-        end
-        );
-    end)
-
-    # set up relayout
-    relayout_obs = Observable(widget, "relayout_args", RelayoutArgs())
-    p.api_obs["relayout"] = relayout_obs
-
-    onjs(relayout_obs, @js function(val)
-        @var gd = this.dom.querySelector("#theplot");
-        Plotly.relayout(gd, val.data).then(function(gd)
-            Plotly.toImage(gd, $(Dict("format" => "svg")))
-        end
-        ).then(function(data)
-            @var svg_data = data.replace("data:image/svg+xml,", "")
-            $svg_obs[] = decodeURIComponent(svg_data)
-        end
-        );
-    end)
-
-    # set up update
-    update_obs = Observable(widget, "update_args", UpdateArgs())
-    p.api_obs["update"] = update_obs
-
-    onjs(update_obs, @js function(val)
-        @var gd = this.dom.querySelector("#theplot");
-        Plotly.update(gd, val.data, val.layout, val.traces).then(function(gd)
-            Plotly.toImage(gd, $(Dict("format" => "svg")))
-        end
-        ).then(function(data)
-            @var svg_data = data.replace("data:image/svg+xml,", "")
-            svg = decodeURIComponent(svg_data)
-            $svg_obs[] = svg
-        end
-        );
-    end)
-
-    # TODO: addtraces, deletetraces, movetraces, redraw, purge, to_image,
-    # download_image, extendtraces, prependtraces
-
+    # TODO
     style = Dict(
         # "width" => "100%",
         # "height" => "100vh",
@@ -106,7 +202,7 @@ function render(p::WebIOPlot)
         # "margin-top" => "0vh",
     )
 
-    widget(dom"div#theplot"(style=style))
+    p.widget(dom"div"(id=string("plot-", p.p.divid), style=style))
 end
 
 
